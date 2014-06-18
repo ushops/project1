780acb927b7c5cd9f7ace43152158a2320bd5633
Alex <alex@ushops.co.il>
2014-06-18 16:56:13 +0300

    ttt5

--- manage/sync.php
+++ manage/sync.php
@@ -1,5 +1,5 @@
 <?php
 
-echo shell_exec("rsync -avz --delete --exclude '.git1' --exclude 'manage' /var/www/html/test.usaddress.com/ /var/www/html/usaddress.com");
+echo shell_exec("rsync -avz --delete --exclude '.git' --exclude 'manage' /var/www/html/test.usaddress.com/ /var/www/html/usaddress.com");
 
 ?>
