8a3a044c8cb99f624c20ad4ca1a7c55a5c86459c
Alex <alex@ushops.co.il>
2014-06-18 16:23:15 +0300

    sync changed

--- sync.php
+++ sync.php
@@ -1,5 +1,5 @@
 <?php
 
-shell_exec('rsync /var/www/html/test.usaddress.com /var/www/html/usaddress.com');
+echo shell_exec('rsync /var/www/html/test.usaddress.com /var/www/html/usaddress.com');
 
 ?>
