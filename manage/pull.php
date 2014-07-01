ad9465b3b5ba0f504d2f3803217018104e45290b
Alex <alex@ushops.co.il>
2014-07-01 18:41:08 +0300

    sdsdg

--- manage/pull.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo shell_exec('git pull git://github.com/ushops/project1');
-?>
--- manage/sync.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo shell_exec("rsync -avz --delete --exclude '.git' --exclude 'manage' /var/www/html/test.usaddress.com/ /var/www/html/usaddress.com");
-
-?>
